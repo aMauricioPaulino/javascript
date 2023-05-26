aac8b98bbdca42cae07c8dfaa97b420320942f0b
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-05-26 19:12:26 -0300

    wip

--- inicio/sentenca_2.js
+++ inicio/sentenca_2.js
@@ -1,2 +1,4 @@
 console.log (1 + 8);
-// Ordem de linhas para a sentenças importam para que os passos possam ser executador corretamente
+// Ordem de linhas para a sentenças importam para que os passos possam ser executador corretamente.
+// console.log é como se fosse um "escreva" no pseudocódigo, ele vai imprimir na tela o que tiver dentro.
+// Para fazer um comentário de múltiplas linha é só usar o (/**/) usados ára fazer comentários tn, em html
