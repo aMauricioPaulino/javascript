ac0a61874f126064ee1ae0699a8e4f454b8975a2
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-05-27 05:49:29 -0300

    mais sobre variáveis

---
+++ inicio/dados_3.js
@@ -0,0 +1,17 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade); // Está mostrando que tipo de variável ela é.
+
+// Mais uma exemplo:
+
+let salario = 4578.32; // Isso é chamado de número com ponto flutuante. Ou seja, ele pode flutuar por todo o número dando diferentes valores a ele.
+console.log(typeof salario); // Repare que diferente do pseudocódigo aqui no JS não tem diferença entre inteiro e real, todos são da variável (NUMBER).
+
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo); // Lembrando que (TYPEOF) está sendo usado para identificar que tipo de variável é.
+
+console.log(typeof "Teste"); // Nesse caso aqui ele mesmo sem declarar a variável ele ainda sim vai identificar qual o tipo ela é mesmo sendo literal.
+
+// Uma obs:
+console.log(typeof 'Teste'); // Para usar valores literais, ou seja imprimir alguma palavra ou tbm posso usar (' ').
