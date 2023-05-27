3baefb4a837497404b3f0f321fb8a7414d4275ef
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-05-27 06:44:44 -0300

    wip

---
+++ inicio/dados_4.js
@@ -0,0 +1,40 @@
+// Uma obs:
+
+let a = 4;
+
+// Aqui está sendo representada uma sentença dentro do código.
+
+// aqui a mesma coisa
+
+// Por algum motivo eu não quero mais que (a seja = a 4), eu posso ir aqui e mdá outro valor para ela, pq lembrando ela é uma variável.
+
+a = 7;
+console.log(a);
+
+
+
+
+// Outro exemplo:
+
+a =  a + 10 ;// Na declaração de valor tbm pode ser acrescentado mais valores com equação.
+
+console.log(a); 
+
+
+
+
+// Outro exemplo:
+
+const b = 4; // Se no lugar de let eu usar const, vai querer dizer que agr não é mais uma variável mas se uma constante.
+
+// exemplo de sentença
+
+// mesma coisa
+
+b = 7 // Repare que quando o sistema for rodado vai dar erro pq uma constante não pode ser atribuído valores e nem modificado ao decorrer do código.
+
+console.log(b)
+
+
+
+
---
+++ inicio/desafio_area.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let  raio = 25;
+let areaDaCirc = PI * raio * raio;
+
+console.log (areaDaCirc, "m2");
+
+// Outro exemplo:
+
+areaDaCirc = Math.PI * raio * raio
+console.log(areaDaCirc, "m2");
