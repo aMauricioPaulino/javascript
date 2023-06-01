4212d6208f3585c0530e012a08c9984358711c59
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-05-31 21:33:09 -0300

    wip

---
+++ inicio/operadores/desafio_logicos.js
@@ -0,0 +1,13 @@
+let verdadeiro1 = (2 + 23) / 5 >= 5 && (84 - 7) * 5 !== 8 ;
+let verdadeiro2 = 30 / 3 + 10 === 20 || 50 % 15 <= 2;
+
+
+console.log( verdadeiro1);
+console.log(verdadeiro2, "\n");
+
+
+let falso1 = 2 + 6 !== 8 && 4 / 2 >= 2;
+let falso2 = !(5 * 10 >= 45) || !(42 + 8 === 50);
+
+console.log(falso1);
+console.log(falso2);
