1d540654260a192e606df77db9b04a6dd5506526
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-06-02 06:10:28 -0300

    wip

---
+++ inicio/operadores/ternario.js
@@ -0,0 +1,5 @@
+// Operador ternário tbm é chamado de atribuição condicional.
+
+const hora = 14;
+const saudacao = hora <= 11 ? "Bom dia " : "Boa tarde";
+console.log(saudacao); //OBS: Sempre terá três partes.
