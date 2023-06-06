09cf2cdb09dad662f8b96cbb93bcbdde17af39bd
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-06-06 20:32:43 -0300

    wip

---
+++ inicio/controle/if.js
@@ -0,0 +1,9 @@
+// o (IF) sempre vem acompanhado de uma expressão (TRUE OR FALSE)
+
+if(true) //obs: dar um espaço para mostrar que a sentença a diante é subordinada a anterior.
+   console.log("VAI SER EXECUTADO"); 
+
+if(false) //OBS: não usar ponto e virgula (;) por conta que se colocar a linha 6 no caso vai ser encerrada ali, ou seja, a linha 7 é subordinada da linha 6.
+   console.log("NÃO VAI SER EXECUTADO");
+
+console.log("FIM");
---
+++ inicio/controle/if_2.js
@@ -0,0 +1,11 @@
+const nota = 8;
+const bomComportamento = false;
+
+
+if (nota >= 7 && bomComportamento)
+    console.log("PARABÉNS");
+
+if (nota < 7 || !bomComportamento)
+    console.log("UMA PENA");
+
+console.log("FIM");
