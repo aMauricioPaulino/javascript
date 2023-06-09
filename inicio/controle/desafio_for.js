f9afd88787c82d9050b8bbf3b7e41d9dcae40e2a
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-06-09 11:04:36 -0300

    wip

---
+++ inicio/controle/desafio_for.js
@@ -0,0 +1,6 @@
+for (let i = 7; i >= 3; i --) {
+    console.log(i)
+    
+}
+
+console.log("fim...")
---
+++ inicio/controle/extra_template_strings.js
@@ -0,0 +1,7 @@
+const i = 2;
+const j = 4;
+
+//console.log("i= " + i + "j= " + j);  essa é uma das formas de fazer
+
+console.log(`i = ${i}, j = ${j}`); // Esa é a forma de fazer uma TEMPLATE STRING. DEIXAR ASA COISAS MIAS ORGANIZADAS.
+//OBS: LEMBRAR DO (${...}) PORQUE ISSO QUE VAI FAZER INTERPRETAR COMO JS. E TBM LEMBRAR DAS (``).
