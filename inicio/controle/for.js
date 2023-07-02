96ce59ffb40b83e83a9421846581df29b10c93b1
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-07-02 07:23:35 -0300

    wip

--- inicio/controle/for.js
+++ inicio/controle/for.js
@@ -1,8 +1,5 @@
-
-
-for (let controle = 0; controle <= 10; controle ++) {
-    console.log(controle)
-    
+for (let controle = 0; controle <= 10; controle += 3) {
+  console.log(controle);
 }
 
-console.log("fim...")
+console.log("fim...");
