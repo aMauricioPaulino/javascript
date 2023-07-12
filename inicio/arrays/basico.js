68fe38c1ee951d67ad03d8e80920bb9377cc49e4
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-07-12 20:32:30 -0300

    sip

--- inicio/arrays/basico.js
+++ inicio/arrays/basico.js
@@ -1,7 +1,7 @@
 const notas = [7.8, 6.7, 10, 9.5, 7.8, 5.8];
 
 console.log(notas);
-console.log(notas[2]);
+console.log(notas.join(", "), notas[2], notas[3]);
 console.log(notas[5]);
 
 notas[4] = 8.7; //Aqui podemos modificar os valores dentro do ARRAY com métodos que ja vimos.
