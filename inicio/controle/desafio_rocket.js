c48fbcdd277f7bd21547e0930b3d9414c20c57d2
Maurício Paulino <124411543+aMauricioPaulino@users.noreply.github.com>
2023-07-17 10:10:46 -0300

    treinado if and else

---
+++ inicio/controle/desafio_rocket.js
@@ -0,0 +1,16 @@
+let nota = 67;
+
+if (nota >= 90) {
+  console.log("Sua nota é A");
+}
+if (nota >= 80 && nota <= 89) {
+  console.log("Sua nota é B");
+}
+if (nota >= 70 && nota <= 79) {
+  console.log("Sua nota é C");
+}
+if (nota >= 60 && nota <= 69) {
+  console.log("Sua nota é E");
+} else {
+  console.log("Sua nota é F");
+}
